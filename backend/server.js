3bd5d4f1312287ebaa11980dc5c3736a3fba95b1
marouane zahaoui <marouanezahaoui835@gmail.com>
2022-07-09 23:46:07 +0100

    create goalRoute.js

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,12 @@
 const express=require('express')
 const dotenv=require('dotenv').config()
-const PORT =5000
+const PORT =process.env.PORT || 5000
 
 const app=express()
+
+app.get('/api/goals',(req,res)=>{
+    res.status(200).json({msg:'all goals'})
+})
 app.listen(PORT,()=>{
     console.log('Server connected with the PORT '+PORT)
 })
