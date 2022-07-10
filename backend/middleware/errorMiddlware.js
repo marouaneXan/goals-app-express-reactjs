c4f5cd47b71ef3cf5e1ddb4fad34f68ab8bcfae0
marouane zahaoui <marouanezahaoui835@gmail.com>
2022-07-10 01:01:03 +0100

    fix some error

--- backend/middleware/errorMiddlware.js
+++ backend/middleware/errorMiddlware.js
@@ -1,6 +1,6 @@
 const errorHandler = (err, req, res, next) => {
   const statusCode = res.statusCode ? res.statusCode : 500;
-  res.statusCode(statusCode);
+  res.status(statusCode);
   res.json({
     message: err.message,
     stack: process.env.NODE_ENV === "production" ? null : err.stack,
--- backend/server.js
+++ backend/server.js
@@ -1,15 +1,19 @@
 const express = require("express");
 const dotenv = require("dotenv").config();
+const { errorHandler } = require("./middleware/errorMiddlware");
 const PORT = process.env.PORT || 5000;
 
 const app = express();
 
 //body parser middleware
-app.use(express.json())
-app.use(express.urlencoded({extended:true}))
-
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 app.use("/api/goals", require("./routes/goalRoutes"));
+
+//errors
+app.use(errorHandler);
+
 app.listen(PORT, () => {
   console.log("Server connected with the PORT " + PORT);
 });
