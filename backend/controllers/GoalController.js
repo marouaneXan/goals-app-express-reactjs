b9783e619f349ff14489b6e78ebb9f36f8597ba9
marouane zahaoui <marouanezahaoui835@gmail.com>
2022-07-10 01:25:06 +0100

    connect mongoDB

--- backend/controllers/GoalController.js
+++ backend/controllers/GoalController.js
@@ -1,22 +1,23 @@
-
+const asyncHandler = require("express-async-handler");
 // @desc  Get goals
 // @route  GET api/goals
 // @access Public
-const getGoals=async (req,res)=>{
-    res.status(200).json({msg:'All goals'})
-}
+const getGoals = asyncHandler(async (req, res) => {
+  res.status(200).json({ msg: "All goals" });
+});
 
 // @desc  Set goals
 // @route  POST api/goals
 // @access Private
-const setGoal=async (req,res)=>{
-    if(!req.body.text){
-        res.status(404)
-        throw new Error('please add a text field')
-    }
-    res.status(200).json({msg:'Goal created successfully'})
-}
+const setGoal = asyncHandler(async (req, res) => {
+  if (!req.body.text) {
+    res.status(404);
+    throw new Error("please add a text field");
+  }
+  res.status(200).json({ msg: "Goal created successfully" });
+});
 
-module.exports={
-    getGoals,setGoal
-}
+module.exports = {
+  getGoals,
+  setGoal,
+};
--- backend/server.js
+++ backend/server.js
@@ -2,8 +2,13 @@ const express = require("express");
 const dotenv = require("dotenv").config();
 const { errorHandler } = require("./middleware/errorMiddlware");
 const PORT = process.env.PORT || 5000;
+const connectDB=require('./config/db')
+
+connectDB()
+
 
 const app = express();
+
 
 //body parser middleware
 app.use(express.json());
