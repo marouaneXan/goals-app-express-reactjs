946b78382f2b30c8337a6ba46dc402cd0b2443ee
marouane zahaoui <marouanezahaoui835@gmail.com>
2022-07-10 01:04:51 +0100

    add express-async-handler

--- backend/controllers/GoalController.js
+++ backend/controllers/GoalController.js
@@ -2,14 +2,14 @@
 // @desc  Get goals
 // @route  GET api/goals
 // @access Public
-const getGoals=(req,res)=>{
+const getGoals=async (req,res)=>{
     res.status(200).json({msg:'All goals'})
 }
 
 // @desc  Set goals
 // @route  POST api/goals
 // @access Private
-const setGoal=(req,res)=>{
+const setGoal=async (req,res)=>{
     if(!req.body.text){
         res.status(404)
         throw new Error('please add a text field')
