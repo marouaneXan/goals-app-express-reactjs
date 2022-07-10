d20968ccd66788fc4048e00c9ed1e5d727734075
marouane zahaoui <marouanezahaoui835@gmail.com>
2022-07-10 18:07:06 +0100

    check for empty fields

--- backend/controllers/UserController.js
+++ backend/controllers/UserController.js
@@ -8,11 +8,17 @@ const bcrypt=require('bcryptjs')
 // @access public
 const registerUser=asyncHandler(async (req,res)=>{
     const {name,email,password}=req.body
+    //check for empty fields
     if(!name || !email || password){
         res.status(400)
         throw new Error('Please add all fields')
     }
-
+    //check if user exist
+    const userExist=user.findOne({email})
+    if(userExist){
+        res.status(400)
+        throw new Error('User Already exist')
+    }
     res.json({msg:'register user'})
 })
 
