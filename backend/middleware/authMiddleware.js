0d9a2bf5d729649ca6ef987c2d29c82857feccd4
marouane zahaoui <marouanezahaoui835@gmail.com>
2022-07-10 20:58:45 +0100

    Verify token

--- backend/middleware/authMiddleware.js
+++ backend/middleware/authMiddleware.js
@@ -8,6 +8,7 @@ const protect=asyncHandler(async (req,res,next)=>{
         // Get token from header
         token=req.headers.authorization.split(' ')[1]
         //Verify token
+        const decoded=jwt.verify(token,process.env.JWT_SECRET)
     }
 })
 
