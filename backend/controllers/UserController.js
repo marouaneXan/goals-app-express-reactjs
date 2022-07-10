00a1c8b615df75a8a8714ced5fdc35624ee0c1ff
marouane zahaoui <marouanezahaoui835@gmail.com>
2022-07-10 18:03:33 +0100

    check for empty fields

--- backend/controllers/UserController.js
+++ backend/controllers/UserController.js
@@ -1,10 +1,18 @@
 const asyncHandler = require("express-async-handler");
 const user = require("../models/userModel");
+const jwt = require('jsonwebtoken')
+const bcrypt=require('bcryptjs')
 
 // @desc Register user
 // @route POST api/user
 // @access public
 const registerUser=asyncHandler(async (req,res)=>{
+    const {name,email,password}=req.body
+    if(!name || !email || password){
+        res.status(400)
+        throw new Error('Please add all fields')
+    }
+
     res.json({msg:'register user'})
 })
 
