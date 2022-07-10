6fde91e5a5d798f5ec188916eed417d948ad383e
marouane zahaoui <marouanezahaoui835@gmail.com>
2022-07-10 21:22:50 +0100

    protect route for user informations

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const router = express.Router()
 const {registerUser,loginUser,getMe} = require('../controllers/UserController')
+const {protect} = require('../middleware/authMiddleware')
 
 //Register
 router.post('/register',registerUser)
@@ -9,6 +10,6 @@ router.post('/register',registerUser)
 router.post('/login',loginUser)
 
 //get user informations
-router.get('/',getMe)
+router.get('/',protect,getMe)
 
 module.exports=router
