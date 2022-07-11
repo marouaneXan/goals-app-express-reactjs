5683bbaebea45a08f5caa147faa477f6be096cfa
marouane zahaoui <marouanezahaoui835@gmail.com>
2022-07-11 21:10:41 +0100

    create register request

---
+++ frontend/src/features/auth/authService.js
@@ -0,0 +1,15 @@
+import axios from "axios";
+
+const API_URL='/api/users/'
+
+//Register user
+const register=async (userData)=>{
+    const res=await axios.post(`API_URL/${register}`,userData)
+    if(res.data){
+        //Set the user data in the localStorage
+        localStorage.setItem('user',JSON.stringify(res.data))
+    }
+    return res.data;
+}
+
+const 
--- frontend/src/features/auth/authSlice.js
+++ frontend/src/features/auth/authSlice.js
@@ -9,3 +9,32 @@ const initialState={
     isLoading:false,
     message:''
 }
+
+//Register user
+
+export const register =createAsyncThunk('auth/register',async (user,thunkAPI)=>{
+    try{
+        return await authService.register(user)
+    }catch(err){
+
+    }
+})
+
+export const authSlice=createSlice({
+    name:'auth',
+    initialState,
+    reducers:{
+        reset:(state)=>{
+            state.isLoading=false
+            state.isError=false
+            state.isSuccess=false
+            state.message=false
+        }
+    },
+    extraReducers:()=>{
+
+    }
+})
+
+export const {reset} =authSlice.actions
+export default authSlice.reducer
