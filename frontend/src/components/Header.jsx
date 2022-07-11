23c1dbad4f198de5c0b5c0802b45ccab857ab8d0
marouane zahaoui <marouanezahaoui835@gmail.com>
2022-07-11 19:01:56 +0100

    add Header component

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,9 +1,27 @@
-import React from 'react'
-
+import {FaSignInAlt,FaSignOutAlt,FaUser} from 'react-icons/fa'
+import {Link} from 'react-router-dom'
 const Header = () => {
   return (
     <div>
-        
+        <div className="header">
+            <div className="logo">
+                <Link to='/'>
+                    Goal Setter 
+                </Link>
+            </div>
+            <ul>
+                <li>
+                    <Link to='/login'>
+                        <FaSignInAlt/>Login
+                    </Link>
+                </li>
+                <li>
+                    <Link to='/register'>
+                        <FaUser/>Register
+                    </Link>
+                </li>
+            </ul>
+        </div>
     </div>
   )
 }
