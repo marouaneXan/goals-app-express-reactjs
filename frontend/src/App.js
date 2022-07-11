53fc97495ae80061475ed1ca48afff2fde4ab382
marouane zahaoui <marouanezahaoui835@gmail.com>
2022-07-11 18:53:53 +0100

    add routes for pages

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,21 @@
-
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Dashboard from './pages/Dashboard'
+import Login from './pages/Login'
+import Register from './pages/Register'
 function App() {
   return (
-    <div>
-      <h1>My App</h1>
-    </div>
+    <>
+      <Router>
+      <div className="container">
+        <Routes>
+          <Route path="/" element={<Dashboard/>} />
+          <Route path="/login" element={<Login/>} />
+          <Route path="/register" element={<Register/>} />
+        </Routes>
+        
+      </div>
+      </Router>
+    </>
   );
 }
 
